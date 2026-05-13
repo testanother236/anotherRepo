d8ed4db1f218241e759dfe613003a6e7a6853188
test test <anastasia.akulenko@succraft.com>
2026-05-13 16:01:25 +0000

    Devops sync. A branch is synced.
    Synced devops commits:
    
    Hash: 75d2d5e0ab5d28750ce276ee3957eb46b46be235
    Message: test
    CreatedAt: 2026-05-13T15:59:31.340Z

--- force-app/main/default/aura/forgotPassword/forgotPasswordHelper.js
+++ force-app/main/default/aura/forgotPassword/forgotPasswordHelper.js
@@ -6,7 +6,7 @@
     handleForgotPassword: function (component, event, helpler) {
         var username = component.find("username").get("v.value");
         var checkEmailUrl = component.get("v.checkEmailUrl");
-        var action = component.get("c.forgotPassowrd");
+        var action = component.get("c.forgotPassword");
         action.setParams({username:username, checkEmailUrl:checkEmailUrl});
         action.setCallback(this, function(a) {
             var rtnValue = a.getReturnValue();
