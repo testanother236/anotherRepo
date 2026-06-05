912f79421f8d14d9691c0eeef804c29a986d958a
test test <anastasia.akulenko@succraft.com>
2026-06-05 15:57:18 +0000

    Devops sync. A branch is synced.
    Synced devops commits:
    
    Hash: 364971e22fd73fb37c7b8c71b3d7867ec2ca1d10
    Message: test
    CreatedAt: 2026-05-29T08:07:15.033Z
    
    Hash: fe9356cb7750c6becf17435b1bb80962976ef967
    Message: test
    CreatedAt: 2026-05-13T16:02:44.124Z
    
    Hash: 88d431d4414ed04178a4cf29010896e1ad0f314b
    Message: test
    CreatedAt: 2026-05-13T15:59:59.622Z
    
    Hash: 75d2d5e0ab5d28750ce276ee3957eb46b46be235
    Message: test
    CreatedAt: 2026-05-13T15:59:31.340Z
    
    Hash: 380b6522473955450466a3dc379ec2eaaf2a2924
    Message: Merge branch
    CreatedAt: 2026-05-13T15:56:13.518Z
    
    Hash: 2f31cb0ad9887d848a41e9e079595f0950b1d6aa
    Message: test
    CreatedAt: 2026-04-17T11:41:26.536Z
    
    Hash: 2acd095f27ec3403c4cc762e883ba145c744b25a
    Message: test
    CreatedAt: 2026-04-17T11:39:51.533Z
    
    Hash: a4be13c2e9f34020d729e71473653c226e21a2a8
    Message: test
    CreatedAt: 2026-04-17T11:38:24.035Z
    
    Hash: dd57d38f1ee8f97480b1803a77474c64cc56629b
    Message: test
    CreatedAt: 2026-03-17T15:16:14.298Z
    
    Hash: 5b00b9dac8a9cc4d381244ddfdef5f4cf2637323
    Message: new check
    CreatedAt: 2026-03-02T18:09:53.745Z
    
    Hash: 59a5479eb982a29d3039ea3fd16f0923aafa8d8b
    Message: Еуые
    CreatedAt: 2026-02-25T14:55:46.848Z
    
    Hash: e2321e1d96f409fc30d032d8846d3862b378006d
    Message: test
    CreatedAt: 2026-02-11T15:16:55.943Z
    
    Hash: 145a5f2534e11c11e4ef0f632d05605507bca5e4
    Message: test
    CreatedAt: 2026-01-28T15:30:07.139Z
    
    Hash: 1eebe9f5201ab25daad214027d75687f89198bd8
    Message: test
    CreatedAt: 2026-01-19T16:04:53.145Z
    
    Hash: c080e20d6a30b8686512c01867fbe163668bcf4e
    Message: test
    CreatedAt: 2026-01-19T15:55:26.755Z
    
    Hash: 7e1616da010c0b1b9efa028d71ac0bb75b1d7b0e
    Message: Merge branch
    CreatedAt: 2026-01-19T15:55:00.646Z
    
    Hash: f6644d9e78f377baf0231ce49952defa9ca6f618
    Message: test
    CreatedAt: 2026-01-19T15:55:00.646Z
    
    Hash: 4b05b70cab7d474c99c3be9fa791b0d97616005f
    Message: test
    CreatedAt: 2026-01-19T15:55:00.646Z
    
    Hash: 1a66eb281780245a310a3139391b39af37617d71
    Message: Init commit
    CreatedAt: 2026-01-19T15:55:00.646Z

---
+++ force-app/main/default/lwc/cfCpqCartInlineError/cfCpqCartInlineError.js
@@ -0,0 +1,72 @@
+import { FlexCardMixin } from "vlocity_cmt/flexCardMixin";
+    import { CurrentPageReference } from 'lightning/navigation';
+    import {interpolateWithRegex, interpolateKeyValue, loadCssFromStaticResource } from "vlocity_cmt/flexCardUtility";
+    
+          import { LightningElement, api, track, wire } from "lwc";
+          import pubsub from "vlocity_cmt/pubsub";
+          import { getRecord } from "lightning/uiRecordApi";
+          
+          import data from "./definition";
+          
+          import styleDef from "./styleDefinition";
+              
+          export default class cfCpqCartInlineError extends FlexCardMixin(LightningElement){
+              currentPageReference;        
+              @wire(CurrentPageReference)
+              setCurrentPageReference(currentPageReference) {
+                this.currentPageReference = currentPageReference;
+              }
+              @api debug;
+              @api recordId;
+              @api objectApiName;
+              
+              @track record;
+              @track _sessionApiVars = {};
+              @track Label={CPQAsyncDiscountPopover:"If you have large number of items, choose the async options to apply discounts asynchronously.",
+        CPQAsyncDiscount:"Async discount",
+        CPQMissingAttributesInfoMessageOrder:"Some products require additional information. You need to fill out all the required attributes for the selected products to submit an order.",
+        CPQMissingAttributesInfoMessageQuote:"Some products require additional information. You need to fill out all the required attributes for the selected products to create an order.",
+        CPQMissingAttributesInfoMessageOppty:"Some products require additional information. You need to fill out all the required attributes for the selected products to create an quote.",
+        CPQMissingAttributes:"Missing Attributes"
+        };
+              pubsubEvent = [];
+              customEvent = [];
+              
+              connectedCallback() {
+                super.connectedCallback();
+                this.setThemeClass(data);
+                this.setStyleDefinition(styleDef);
+                data.Session = {} //reinitialize on reload
+                
+                
+                this.customLabels = this.Label;
+                      
+                          this.fetchUpdatedCustomLabels();
+                      
+                this.setDefinition(data);
+ this.registerEvents();
+                
+                
+              }
+              
+              disconnectedCallback(){
+                super.disconnectedCallback();
+                    
+                    
+
+                  this.unregisterEvents();
+              }
+
+              registerEvents() {
+                
+              }
+
+              unregisterEvents(){
+                
+              }
+            
+              renderedCallback() {
+                super.renderedCallback();
+                
+              }
+          }
